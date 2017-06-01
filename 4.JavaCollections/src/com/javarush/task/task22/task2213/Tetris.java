502ea2fd3c6266928792f7922f080ae440aa0439
Victor Sharygin <lasprog@mail.ru>
2017-06-02 00:12:27 +0300

    task2213 (2...5)

--- 4.JavaCollections/src/com/javarush/task/task22/task2213/Tetris.java
+++ 4.JavaCollections/src/com/javarush/task/task22/task2213/Tetris.java
@@ -4,7 +4,28 @@ package com.javarush.task.task22.task2213;
  * Created by dell on 01-Jun-17.
  */
 public class Tetris {
+    private Field field;
+    private Figure figure;
+    public static Tetris game;
+
     public static void main(String[] args) {
+        game = new Tetris();
+        game.run();
+    }
+
+    public Field getField() {
+        return field;
+    }
+
+    public Figure getFigure() {
+        return figure;
+    }
+
+    public  void run() {
+
+    }
+
+    public void step() {
 
     }
 }
