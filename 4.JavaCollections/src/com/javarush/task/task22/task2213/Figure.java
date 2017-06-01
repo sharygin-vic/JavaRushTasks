810fe816dd098fb32c93fccb24b7c0ef6ec9075b
Victor Sharygin <lasprog@mail.ru>
2017-06-02 00:00:52 +0300

    task2213-1

---
+++ 4.JavaCollections/src/com/javarush/task/task22/task2213/Figure.java
@@ -0,0 +1,8 @@
+package com.javarush.task.task22.task2213;
+
+/**
+ * Created by dell on 01-Jun-17.
+ */
+public class Figure {
+
+}
---
+++ 4.JavaCollections/src/com/javarush/task/task22/task2213/Tetris.java
@@ -0,0 +1,10 @@
+package com.javarush.task.task22.task2213;
+
+/**
+ * Created by dell on 01-Jun-17.
+ */
+public class Tetris {
+    public static void main(String[] args) {
+
+    }
+}
