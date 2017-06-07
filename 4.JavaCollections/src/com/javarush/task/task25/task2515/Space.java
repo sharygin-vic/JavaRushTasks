d808bafcb5b570128381497d12b32c8648649ddf
Victor Sharygin <lasprog@mail.ru>
2017-06-07 23:40:51 +0300

    task2515 (1...3)

---
+++ 4.JavaCollections/src/com/javarush/task/task25/task2515/Space.java
@@ -0,0 +1,54 @@
+package com.javarush.task.task25.task2515;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by dell on 07-Jun-17.
+ */
+public class Space {
+    private int width;
+    private int height;
+    private SpaceShip ship;
+    private ArrayList<Ufo> ufos = new ArrayList<Ufo>();
+    private ArrayList<Rocket> rockets = new ArrayList<Rocket>();
+    private ArrayList<Bomb> bombs = new ArrayList<Bomb>();
+
+    public Space(int width, int height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public SpaceShip getShip() {
+        return ship;
+    }
+
+    public ArrayList<Ufo> getUfos() {
+        return ufos;
+    }
+
+    public ArrayList<Rocket> getRockets() {
+        return rockets;
+    }
+
+    public ArrayList<Bomb> getBombs() {
+        return bombs;
+    }
+
+
+    public void setShip(SpaceShip ship) {
+        this.ship = ship;
+    }
+}
---
+++ 4.JavaCollections/src/com/javarush/task/task25/task2515/SpaceShip.java
@@ -0,0 +1,8 @@
+package com.javarush.task.task25.task2515;
+
+/**
+ * Created by dell on 07-Jun-17.
+ */
+public class SpaceShip extends BaseObject {
+
+}
