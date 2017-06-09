4f08b4eb861609c859d969ca4c2ed3b5dfdd586d
Victor Sharygin <lasprog@mail.ru>
2017-06-10 01:47:24 +0300

    task3707 (2)

--- 4.JavaCollections/src/com/javarush/task/task37/task3707/AmigoSet.java
+++ 4.JavaCollections/src/com/javarush/task/task37/task3707/AmigoSet.java
@@ -1,35 +1,35 @@
 package com.javarush.task.task37.task3707;
 
 import java.io.Serializable;
-import java.util.AbstractSet;
-import java.util.Collection;
-import java.util.Iterator;
-import java.util.Set;
+import java.util.*;
 
 /**
  * Created by dell on 09-Jun-17.
  */
 public class AmigoSet<E> extends AbstractSet<E> implements Serializable, Cloneable, Set<E> {
+    private static final Object PRESENT = new Object();
+    private transient HashMap<E,Object> map;
+
+    public AmigoSet() {
+        map = new HashMap<E,Object>();
+    }
+
+    public AmigoSet(Collection<? extends E> collection) {
+        int initialCapacity = Math.max(16, (int)(collection.size() / 0.75)+1);
+        map = new HashMap<E,Object>(initialCapacity);
+        addAll(collection);
+    }
+
+    @Override
+    public boolean add(E e) {
+        return null == map.put(e, PRESENT);
+    }
 
     @Override
     public Iterator<E> iterator() {
         return null;
     }
 
-    @Override
-    public boolean add(Object o) {
-        return false;
-    }
-
-    @Override
-    public boolean addAll(Collection c) {
-        return false;
-    }
-
-    @Override
-    public Object[] toArray(Object[] a) {
-        return new Object[0];
-    }
 
     @Override
     public int size() {
