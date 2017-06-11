41ccf0ab28146febc6b0652c88176db85e73a461
Victor Sharygin <lasprog@mail.ru>
2017-06-11 21:35:09 +0300

    task3505

---
+++ 4.JavaCollections/src/com/javarush/task/task35/task3505/ConvertableUtil.java
@@ -0,0 +1,16 @@
+package com.javarush.task.task35.task3505;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class ConvertableUtil {
+
+    public static <K, V extends Convertable<K>> Map<K, V> convert(List<V> list) {
+        Map<K, V> result = new HashMap<K, V>();
+        for (V item : list) {
+            result.put(item.getKey(), item);
+        }
+        return result;
+    }
+}
