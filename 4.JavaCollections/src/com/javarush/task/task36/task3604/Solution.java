0af689d1d4e31cc95ecdd4c95453c233e5dad60c
Victor Sharygin <lasprog@mail.ru>
2017-06-14 03:28:47 +0300

    task3604

---
+++ 4.JavaCollections/src/com/javarush/task/task36/task3604/Solution.java
@@ -0,0 +1,32 @@
+package com.javarush.task.task36.task3604;
+
+/* 
+Разбираемся в красно-черном дереве
+*/
+public class Solution {
+    public static void main(String[] args) {
+        RedBlackTree redBlackTree = new RedBlackTree();
+        redBlackTree.insert(5);
+        redBlackTree.insert(2);
+        redBlackTree.insert(9);
+        redBlackTree.insert(4);
+        redBlackTree.insert(1);
+        redBlackTree.insert(10);
+        redBlackTree.insert(7);
+        redBlackTree.insert(3);
+        redBlackTree.insert(8);
+        redBlackTree.insert(6);
+
+
+//        redBlackTree.insert(1);
+//        redBlackTree.insert(2);
+//        redBlackTree.insert(3);
+//        redBlackTree.insert(4);
+//        redBlackTree.insert(5);
+//        redBlackTree.insert(6);
+//        redBlackTree.insert(7);
+//        redBlackTree.insert(8);
+//        redBlackTree.insert(9);
+//        redBlackTree.insert(10);
+    }
+}
