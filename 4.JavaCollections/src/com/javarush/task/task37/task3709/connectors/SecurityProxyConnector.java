275678ebac69fd18fdfdc78a55edebac097c9595
Victor Sharygin <lasprog@mail.ru>
2017-06-19 01:45:22 +0300

    task3709

---
+++ 4.JavaCollections/src/com/javarush/task/task37/task3709/connectors/SecurityProxyConnector.java
@@ -0,0 +1,24 @@
+package com.javarush.task.task37.task3709.connectors;
+
+import com.javarush.task.task37.task3709.security.SecurityChecker;
+import com.javarush.task.task37.task3709.security.SecurityCheckerImpl;
+
+/**
+ * Created by dell on 19-Jun-17.
+ */
+// vic :
+public class SecurityProxyConnector implements Connector {
+    private SimpleConnector simpleConnector;
+    private SecurityChecker securityChecker = new SecurityCheckerImpl();
+
+    public SecurityProxyConnector(String resourceString) {
+        this.simpleConnector = new SimpleConnector(resourceString);
+    }
+
+    @Override
+    public void connect() {
+        if (securityChecker.performSecurityCheck()) {
+            simpleConnector.connect();
+        }
+    }
+}
