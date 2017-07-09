ad7c6b80f5b4be8b76f019ff74c56d2406bb3066
Victor Sharygin <lasprog@mail.ru>
2017-07-09 03:42:09 +0300

    task2810 (3)

---
+++ 4.JavaCollections/src/com/javarush/task/task28/task2810/model/HHStrategy.java
@@ -0,0 +1,9 @@
+package com.javarush.task.task28.task2810.model;
+
+/**
+ * Created by dell on 09-Jul-17.
+ */
+
+//http://hh.ua/ и http://hh.ru/
+public class HHStrategy implements Strategy {
+}
--- 4.JavaCollections/src/com/javarush/task/task28/task2810/model/Provider.java
+++ 4.JavaCollections/src/com/javarush/task/task28/task2810/model/Provider.java
@@ -1,5 +1,9 @@
 package com.javarush.task.task28.task2810.model;
 
+import com.javarush.task.task28.task2810.vo.Vacancy;
+
+import java.util.List;
+
 /**
  * Created by dell on 09-Jul-17.
  */
@@ -12,5 +16,9 @@ public class Provider {
 
     public void setStrategy(Strategy strategy) {
         this.strategy = strategy;
+    }
+
+    public List<Vacancy> getJavaVacancies(String searchString) {
+        return null;
     }
 }
