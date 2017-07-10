ae3c8e538490344b6cfaa91c01528d2927c681db
Victor Sharygin <lasprog@mail.ru>
2017-07-10 16:47:15 +0300

    task2810 (6)

--- 4.JavaCollections/src/com/javarush/task/task28/task2810/model/HHStrategy.java
+++ 4.JavaCollections/src/com/javarush/task/task28/task2810/model/HHStrategy.java
@@ -1,7 +1,10 @@
 package com.javarush.task.task28.task2810.model;
 
 import com.javarush.task.task28.task2810.vo.Vacancy;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -14,11 +17,20 @@ import java.util.List;
 // https://hh.ua/search/vacancy?text=java+ADDITIONAL_VALUE&page=PAGE_VALUE
 public class HHStrategy implements Strategy {
     private static final String URL_FORMAT = "http://hh.ua/search/vacancy?text=java+%s&page=%d";
+    private static final String userAgent = "Mozilla/5.0 (jsoup)";
+    private static final int timeout = 5 * 1000;
 
     @Override
     public List<Vacancy> getVacancies(String searchString) {
         ArrayList<Vacancy> res = new ArrayList<Vacancy>();
-
+        Document doc;
+        try {
+            doc = Jsoup.connect(String.format(URL_FORMAT, searchString, 0))
+                    .get();
+            //System.out.println(doc.html());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
         return res;
     }
