3bf2848f02ba6fec51fdaf218a028c13d564da7e
Victor Sharygin <lasprog@mail.ru>
2017-07-24 04:41:32 +0300

    task2209

--- 3.JavaMultithreading/src/com/javarush/task/task22/task2209/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/task22/task2209/Solution.java
@@ -1,67 +1,65 @@
 package com.javarush.task.task22.task2209;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.nio.charset.Charset;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.*;
 
 /*
 Составить цепочку слов
 */
 public class Solution {
-    private static char utf8char = 0;
-    private static String wordWithUtf8Char = "";
+    private static char[] utf8chars = {'\uFEFF', '\uFFFE'};
 
     public static void main(String[] args)  {
-        //...
-        ArrayList<String> words = new ArrayList<String>();
-//          String fileName = "c:\\r\\1.txt";
         try (
-            BufferedReader r = new BufferedReader(new InputStreamReader(System.in));
-
-            BufferedReader fr = new BufferedReader(new FileReader(r.readLine()));
+                BufferedReader r = new BufferedReader(new InputStreamReader(System.in));
         ) {
-            while (fr.ready()) {
-                String line = fr.readLine();
-                String[] ss;
-                if (line.charAt(0) == '\uFEFF' || line.charAt(0) == '\uFFFE') {
-                    ss = line.substring(1).split(" ");
-                    utf8char = '\uFEFF';
-                    wordWithUtf8Char = ss[0];
-                } else {
-                    ss = line.split(" ");
-                }
-                for (String s : ss) {
-                    if (s.trim().length() > 0) {
-                        words.add(s);
-                    }
-                }
+            String content = new String(Files.readAllBytes(Paths.get(r.readLine())), StandardCharsets.UTF_8);
+
+            // FOR DEBUG:
+            //String content = null;
+            //String content = "";
+            //String content = "Киев Нью-Йорк Амстердам\n Вена   Мельбурн";
+            //String content = "\uFEFFКиев   Нью-Йорк    Амстердам  Мельбурн Чес Монегем воронеЖ Жмеринка Артемовск Суворовка Капо  Маев Вена Аеку  Удрот Треля Яс   Срача Апер Ровно   Олем";
+            //String content = "Киев   Нью-Йорк    Амстердам  Мельбурн Чес Монегем Воронеж Жмеринка Артемовск Суворовка Капо  Маев Вена Аеку  Удрот Треля Яс   Срача Апер Ровно   Олем";
+//            String content = "\uFEFFКиев   Нью-Йорк    Амстердам  Мельбурн Чес Монегем воронеЖ Жмеринка Артемовск Суворовка " +
+//                    "Капо  Маев Вена Аеку  Удрот Треля Яс   Срача Апер Ровно " +
+//                    "  Олем  Отребово образ Забор Рожеве Ерепейск кротинская яблочное Ефремка Алупка " +
+//                    "Аспер ролда адагарка арпа астер родня яворино орлово Ольгин Никополье " +
+//                    " Ежов витебск кровск кубард дромив волф форка алпуг гундота атлас " +
+//                    " серж жопа апупеня Явор-Пришиб берег груз забор роза астахово орел";      // 60 words -> work time = 1..2 sec
+
+            if (content.charAt(0) == utf8chars[0] || content.charAt(0) == utf8chars[1]) {
+                content = content.substring(1);
             }
-
-            System.out.println(Charset.defaultCharset());
-
-            StringBuilder result = getLine(words.toArray(new String[0]));
+            String[] words = content.split("\\s+");
+            StringBuilder result = getLine(words);
             System.out.println(result.toString());
         }
         catch (IOException e) {
-            StringBuilder result = getLine(words.toArray(new String[0]));
-            System.out.println(result.toString());
+            e.printStackTrace();
         }
     }
 
     public static StringBuilder getLine(String... words) {
         StringBuilder sbRes = new StringBuilder();
+
+        if (words == null || words.length == 0)
+            return sbRes;
+        if (words.length == 1 || words[0].equals(""))
+            return sbRes.append(words[0]);
+
         int sbResCountWords = 0;
         ArrayList<String> listSrc = new ArrayList<String>(Arrays.asList(words));
 
-        for (int shufleCur = 0; shufleCur < 100000 * listSrc.size(); shufleCur++) {
+        for (int shufleCur = 0; shufleCur < 1000 * listSrc.size(); shufleCur++) {
             ArrayList<String> list = new ArrayList<String>(listSrc);
 
             Collections.shuffle(list, new Random());
             StringBuilder sb = new StringBuilder();
-            int sbCountWords = 0;
+            int sbCountWords = 1;
 
             int prevIterWordsCount = list.size();
             int curIterWordsCount = prevIterWordsCount;
@@ -77,21 +75,15 @@ public class Solution {
                 } else {
                     char srsWordStart = srsWord.charAt(0);
                     char sbEnd = sb.charAt(sb.length() - 1);
-                    if (Character.toLowerCase(srsWordStart) == sbEnd) {
+                    if (Character.toLowerCase(srsWordStart) == Character.toLowerCase(sbEnd)) {
                         sb.append(" ");
-                        if (wordWithUtf8Char.equals(srsWord)) {
-                            sb.append(utf8char);
-                        }
                         sb.append(srsWord);
                         sbCountWords++;
                     } else {
                         char srsWordEnd = srsWord.charAt(srsWord.length() - 1);
                         char sbStart = sb.charAt(0);
-                        if (Character.toUpperCase(srsWordEnd) == sbStart) {
+                        if (Character.toUpperCase(srsWordEnd) == Character.toLowerCase(sbStart)) {
                             sb.insert(0, " ").insert(0, srsWord);
-                            if (wordWithUtf8Char.equals(srsWord)) {
-                                sb.insert(0, utf8char);
-                            }
                             sbCountWords++;
                         } else {
                             list.add(srsWord);
@@ -115,7 +107,13 @@ public class Solution {
                 sbRes = sb;
                 sbResCountWords = sbCountWords;
             }
+
+            if (sbResCountWords == listSrc.size()) {
+                break;  // found all words
+            }
         }
+
+        //System.out.println("count = " + (sbResCountWords));   // for debug
 
         return sbRes;
     }
