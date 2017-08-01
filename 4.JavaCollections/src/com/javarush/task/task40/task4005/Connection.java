1d7ae711b596d2530b385051cae77603513f611e
sharygin-vic <lasprog@mail.ru>
2017-08-01 04:44:02 +0300

    task4005

---
+++ 4.JavaCollections/src/com/javarush/task/task40/task4005/Connection.java
@@ -0,0 +1,45 @@
+package com.javarush.task.task40.task4005;
+
+
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+
+/* 
+Сокетный сервер и клиент
+*/
+
+public class Connection implements Closeable {
+    private final Socket socket;
+    private final ObjectInputStream in;
+    private final ObjectOutputStream out;
+
+    public Connection(Socket socket) throws Exception {
+        this.socket = socket;
+
+        // так работает:
+        this.out = new ObjectOutputStream(socket.getOutputStream());    // vic
+        this.in = new ObjectInputStream(socket.getInputStream());       // vic
+
+        // так подвисает
+        //this.in = new ObjectInputStream(socket.getInputStream());     // vic
+        //this.out = new ObjectOutputStream(socket.getOutputStream());  // vic
+    }
+
+    public void send(String message) throws Exception {
+        out.writeObject(message);
+    }
+
+    public String receive() throws Exception {
+        return (String) in.readObject();
+    }
+
+    @Override
+    public void close() throws IOException {
+        in.close();
+        out.close();
+        socket.close();
+    }
+}
